7c0bf29b00d185e04ba80286cbead848660d96fe
Сергей Михайлин <semihal.devs@gmail.com>
2020-06-17 10:55:26 +0300

    Cache flag for dataset read methods

--- utils/read.py
+++ utils/read.py
@@ -1,24 +1,34 @@
+import joblib
+import os
 import pandas as pd
 
 from utils.dtype import downcast
 
 
-def read_sales_dataset(path: str = 'data/raw/sales_train_evaluation.csv'):
-    ds = pd.read_csv(path, index_col='id')
+def read_sales_dataset(path: str = 'data/raw/sales_train_evaluation.csv', use_cache=True):
+    if use_cache and os.path.isfile('data/cache/sales.jbl'):
+        ds = joblib.load('data/cache/sales.jbl')
+        return ds
+
+    ds = pd.read_csv(path)
 
     ds.loc[:, 'constant_id'] = 0
-    # Add zero sales for the remaining days 1942-1969
-    for d in range(1942, 1970):
-        col = 'd_' + str(d)
-        ds[col] = 0
 
     ds = downcast(ds)
     categorical_columns = ['item_id', 'dept_id', 'cat_id', 'store_id', 'state_id']
     ds[categorical_columns] = ds[categorical_columns].astype('category')
+
+    if use_cache:
+        joblib.dump(ds, 'data/cache/sales.jbl')
+
     return ds
 
 
-def read_calendar_dataset(path: str = 'data/raw/calendar.csv'):
+def read_calendar_dataset(path: str = 'data/raw/calendar.csv', use_cache=True):
+    if use_cache and os.path.isfile('data/cache/calendar.jbl'):
+        ds = joblib.load('data/cache/calendar.jbl')
+        return ds
+
     ds = pd.read_csv(path, date_parser=['date'])
     ds = ds.set_index('d')
     ds = downcast(ds)
@@ -29,12 +39,24 @@ def read_calendar_dataset(path: str = 'data/raw/calendar.csv'):
         'snap_CA', 'snap_TX', 'snap_WI'
     ]
     ds[categorical_columns] = ds[categorical_columns].astype('category')
+
+    if use_cache:
+        joblib.dump(ds, 'data/cache/calendar.jbl')
+
     return ds
 
 
-def read_prices_dataset(path: str = 'data/raw/sell_prices.csv'):
+def read_prices_dataset(path: str = 'data/raw/sell_prices.csv', use_cache=True):
+    if use_cache and os.path.isfile('data/cache/prices.jbl'):
+        ds = joblib.load('data/cache/prices.jbl')
+        return ds
+
     ds = pd.read_csv(path)
     ds = downcast(ds)
     categorical_columns = ['store_id', 'item_id']
     ds[categorical_columns] = ds[categorical_columns].astype('category')
+
+    if use_cache:
+        joblib.dump(ds, 'data/cache/prices.jbl')
+
     return ds
