e5bb2d66476896e7daef7221cae12e6b6859848d
Сергей Михайлин <semihal.devs@gmail.com>
2020-06-17 10:55:05 +0300

    Metric work with base dataset (from build_base_dataset)

--- m5/metric.py
+++ m5/metric.py
@@ -2,8 +2,10 @@ from typing import Dict
 
 import pandas as pd
 import numpy as np
+from tqdm import tqdm
 
-from utils.funcs import day_to_week_dict, select_tail_days, only_days_columns
+from baseline import build_base_dataset
+from utils.funcs import only_days_columns
 from m5.constants import *
 
 
@@ -15,46 +17,48 @@ def read_dataset(path: str = 'data/raw/sales_train_evaluation.csv'):
 
 class WRMSSE:
 
-    def __init__(self, train, validate, prices, levels=LEVELS):
-        self.train = train
-        self.validate = validate
+    def __init__(self, train, validate, levels=LEVELS):
+        print('Calculate profit')
+        self.daily_profit = self._daily_profit(train)
+        print('Transform train to standard view')
+        self.train = self._to_standard_view(train)
+        print('Transform test to standard view')
+        self.validate = self._to_standard_view(validate)
         self.validate_days = only_days_columns(self.validate)
-        self.prices = prices
-        self.daily_profit = self._daily_profit()
+        # self.prices = prices
 
+        print('Calculate specification')
         self.levels = levels
         self._levels_spec = self.levels_specifications()
 
-    def _daily_profit(self) -> pd.DataFrame:
-        ds_used_days = select_tail_days(self.train, length=28)
-        ds_with_prices = self._merge_prices(ds_used_days)
-        ds_day_revenue = self._revenue(ds_with_prices)
+    def _daily_profit(self, train) -> pd.DataFrame:
+        # get last 28 days
+        train = train[ID_COLUMNS + ['d', 'sold', 'wm_yr_wk', 'sell_price']]
+        max_day = train['d'].max()
+        ds_last_days = train[train['d'] > max_day - 28]
+        # calculate revenue
+        ds_day_revenue = self._revenue(ds_last_days, train)
         return ds_day_revenue
 
-    def _merge_prices(self, ds: pd.DataFrame) -> pd.DataFrame:
-        # purchase by day
-        ds = ds.set_index(['item_id', 'store_id'])
-        ds_only_days = ds.loc[:, ds.columns.str.startswith('d_')]
-        ds_days_stack = ds_only_days.stack().reset_index()
-        ds_day_purchases = ds_days_stack.rename(columns={'level_2': 'day', 0: 'purchases'})
-        # add wm_yr_wk
-        ds_day_purchases.loc[:, 'wm_yr_wk'] = ds_day_purchases['day'].map(day_to_week_dict())
-        # merge prices
-        ds_purchases_with_prices = ds_day_purchases.merge(
-            self.prices, how='left',
-            on=['item_id', 'store_id', 'wm_yr_wk']
-        )
-        return ds_purchases_with_prices
-
-    def _revenue(self, ds_with_prices: pd.DataFrame):
+    def _revenue(self, ds_with_prices: pd.DataFrame, train: pd.DataFrame):
         # add revenue feature
-        ds_with_prices.loc[:, 'revenue'] = ds_with_prices['purchases'] * ds_with_prices['sell_price']
-        #
-        ds_with_prices = ds_with_prices.set_index(['item_id', 'store_id', 'day'])
-        ds_day_revenue = ds_with_prices.unstack(level=2)['revenue']
-        ds_day_revenue = ds_day_revenue.loc[zip(self.train.item_id, self.train.store_id), :].reset_index(drop=True)
-        ds_item_revenue = pd.concat([self.train[ID_COLUMNS], ds_day_revenue], axis=1, sort=False)
+        ds_with_prices.loc[:, 'revenue'] = ds_with_prices['sold'] * ds_with_prices['sell_price']
+        ds_with_prices = ds_with_prices.set_index(['item_id', 'store_id', 'd'])['revenue']
+        ds_day_revenue = ds_with_prices.unstack(level=2)
+
+        train_index = train[ID_COLUMNS].drop_duplicates()
+        ds_item_revenue = train_index.merge(ds_day_revenue, left_on=['item_id', 'store_id'], right_index=True)
+        rename_dict = {day_num: f'd_{day_num}' for day_num in ds_item_revenue.columns if isinstance(day_num, int)}
+        ds_item_revenue = ds_item_revenue.rename(columns=rename_dict)
         return ds_item_revenue
+
+    def _to_standard_view(self, ds: pd.DataFrame):
+        indexes = ds[ID_COLUMNS].drop_duplicates()
+        pivot = pd.pivot(ds, index='id', columns='d', values='sold').reset_index()
+        view = pivot.merge(indexes, on='id')
+        rename_dict = {day_num: f'd_{day_num}' for day_num in view.columns if isinstance(day_num, int)}
+        view = view.rename(columns=rename_dict)
+        return view
 
     def levels_specifications(self) -> Dict:
         # get columns name
@@ -70,7 +74,7 @@ class WRMSSE:
             for i, _ in enumerate(LEVELS, start=1)
         }
         # calculate specifications
-        for level_id, level in enumerate(self.levels, start=1):
+        for level_id, level in tqdm(enumerate(self.levels, start=1), total=len(self.levels)):
             level_series = self.train.groupby(level)[train_days].sum()
             for _, row in level_series.iterrows():
                 # start of the active period (p. 6)
@@ -116,15 +120,7 @@ class WRMSSE:
 
 
 if __name__ == '__main__':
-    cal = pd.read_csv('../data/raw/calendar.csv')
-    eval_set = read_dataset()
-    prices_set = pd.read_csv('../data/raw/sell_prices.csv')
-
-    day_columns = only_days_columns(eval_set)
-    train_dataset = eval_set.drop(day_columns[-28:], axis=1)
-    val_dataset = eval_set.drop(day_columns[:-28], axis=1)
-    metric = WRMSSE(train_dataset, val_dataset, prices_set, levels=LEVELS[:2])
-
-    predict = eval_set.copy()
-    predict[metric.validate_days] = (np.random.random(predict[metric.validate_days].shape) * 10).astype('int')
-    s = metric.score(predict)
+    ds = build_base_dataset()
+    train_dataset = ds[ds['d'] <= 1913]
+    val_dataset = ds[ds['d'] > 1913]
+    metric = WRMSSE(train_dataset, val_dataset, levels=LEVELS[:2])
